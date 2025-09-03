fcfedd6dd8ae98ee27cde0e84315255a94f2d86b
MartroxOne1 <144127452+MartroxOne1@users.noreply.github.com>
2025-09-03 15:16:50 -0400

    Ultimo Commit Visual

--- ProyectoSushi/src/app/features/carrito/carrito.service.ts
+++ ProyectoSushi/src/app/features/carrito/carrito.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'  // Esto hace que Angular lo inyecte automáticamente
+  providedIn: 'root'
 })
 export class CarritoService {
 
